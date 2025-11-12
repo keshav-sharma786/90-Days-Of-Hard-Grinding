64e6374280e4f961e01e4dfd37fde13448aaf51f
Keshav Sharma <Keshav.bhambi@gmail.com>
2025-11-12 20:17:08 +0530

    initial commit

---
+++ DevTinder/NamasteNodeJSSeason2/routeHandlers.js
@@ -0,0 +1,35 @@
+const express = require("express");
+
+const app = express();
+
+// Order of the code also matters my dear friends
+// app.use("/user", (req, res) => {
+//   res.send("HAHAHAHAHAHAHAHA");
+// });
+
+// This will only handle GET call to /user
+// dynamic routing
+app.get("/user/:userId", (req, res) => {
+  console.log(req.params);
+  res.send({ firstName: "Keshav", lastName: "Sharma" });
+});
+
+// post request
+app.post("/user", (req, res) => {
+  // saving data to DB
+  res.send("Data successfully saved to the database!");
+});
+
+// delete request
+app.delete("/user", (req, res) => {
+  res.send("user deleted successfully!");
+});
+
+// this will match all the HTTP method API calls to /test
+app.use("/test", (req, res) => {
+  res.send("Hello from the server");
+});
+
+app.listen(3000, () => {
+  console.log("Server is successfully listening on port 3000...");
+});
--- DevTinder/src/app.js
+++ DevTinder/src/app.js
@@ -2,35 +2,19 @@ const express = require("express");
 
 const app = express();
 
-// Order of the code also matters my dear friends
-// app.use("/user", (req, res) => {
-//   res.send("HAHAHAHAHAHAHAHA");
-// });
+app.use(
+  "/user",
+  (req, res) => {
+    console.log("Handling the route user!!");
+    //res.send("Response!!!");
+  },
+  (req, res) => {
+    // third argument => route handler2
+    console.log("Handling the route user2!!");
+    res.send("2nd Response!!!");
+  }
+);
 
-// This will only handle GET call to /user
-// dynamic routing
-app.get("/user/:userId", (req, res) => {
-  console.log(req.params);
-  res.send({ firstName: "Keshav", lastName: "Sharma" });
+app.listen(7777, () => {
+  console.log("Server is successfully listening on port 7777...");
 });
-
-// post request
-app.post("/user", (req, res) => {
-  // saving data to DB
-  res.send("Data successfully saved to the database!");
-});
-
-// delete request
-app.delete("/user", (req, res) => {
-  res.send("user deleted successfully!");
-});
-
-// this will match all the HTTP method API calls to /test
-app.use("/test", (req, res) => {
-  res.send("Hello from the server");
-});
-
-app.listen(3000, () => {
-  console.log("Server is successfully listening on port 3000...");
-});
-
