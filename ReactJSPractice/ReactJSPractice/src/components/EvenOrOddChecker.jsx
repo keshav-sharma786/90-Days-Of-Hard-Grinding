116063e9dfcddd1f14e4143bbf07007b673c70d4
Keshav Sharma <Keshav.bhambi@gmail.com>
2025-12-07 19:54:42 +0530

    initial commit

--- ReactJSPractice/ReactJSPractice/src/components/EvenOrOddChecker.jsx
+++ ReactJSPractice/ReactJSPractice/src/components/EvenOrOddChecker.jsx
@@ -3,12 +3,15 @@ import React, { useState } from "react";
 
 function EvenOrOddChecker() {
   const [numberInput, setNumberInput] = useState("");
+  const [result, setResult] = useState(null);
   console.log(numberInput);
+  // writing a separate state for the useState hook
+  const [loading, setLoading] = useState(false);
   console.log(typeof numberInput);
   // let us create a separate function which will basically check the whether the number is even, or odd or invalid
   // let us name that function as the checker
 
-  function isFinite(num) {
+  function isFiniteN(num) {
     if (!Number.isFinite(num)) {
       return false;
     }
@@ -17,11 +20,31 @@ function EvenOrOddChecker() {
 
   function Checker(numberInput) {
     // first we will convert the string into the number by using the Number constructor
-    const isEvenOdd = Number(numberInput);
-    // validating if the user has entered some wrong input
-    // let us create a separate function to check weather the input number is finite or not
-    const isFiniteNum = isFinite(isEvenOdd);
-    
+    // what if the user types a empty string or nothing inside the input box
+    if (numberInput === "") {
+      return;
+    }
+    setLoading(true);
+    setTimeout(() => {
+      console.log(numberInput);
+      const isEvenOdd = Number(numberInput);
+      console.log(numberInput);
+      // validating if the user has entered some wrong input
+      // let us create a separate function to check weather the input number is finite or not
+      const isFiniteNum = isFiniteN(isEvenOdd);
+      // if the input of the user is not valid then in the results we basically have to show "Please enter a valid number"
+      // if(!isFiniteNum) {
+      //   setResult("Please enter a valid number");
+      // } else {
+      //   setResult("You entered a correct number");
+      // }
+      !isFiniteNum
+        ? setResult("Please enter a valid number")
+        : isEvenOdd % 2 === 0
+        ? setResult(`The number ${isEvenOdd} is even`)
+        : setResult(`The number ${isEvenOdd} is odd`);
+      setLoading(false);
+    }, 1000);
   }
   return (
     <div className="even-odd-container w-80 mt-6 flex flex-col justify-center items-center m-auto py-12 border border-amber-900">
@@ -36,12 +59,22 @@ function EvenOrOddChecker() {
         }}
       />
 
-      <button className="check-button h-10 w-30 mt-6  border border-indigo-600 mx-">
+      <button
+        className="check-button h-10 w-30 mt-6  border border-indigo-600 mx-"
+        onClick={() => {
+          // putting a loading delay of 1 second also
+          // setTimeout(() => {
+          //   setResult("Checking ...");
+          // }, 1000);
+          // setResult("checking");
+          Checker(numberInput);
+        }}
+      >
         Check
       </button>
 
       <div className="result-area h-20 w-40 m-auto">
-        <div className="result mt-6">Result appears here</div>
+        <div className="result mt-6">{loading ? "checking..." : result}</div>
       </div>
     </div>
   );
