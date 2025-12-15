4d3c5732b5e742c02fd468b17a4ceac11c9eea47
Keshav Sharma <Keshav.bhambi@gmail.com>
2025-12-15 19:42:41 +0530

    initial commit

--- ReactJSPractice/ReactJSPractice/src/App.jsx
+++ ReactJSPractice/ReactJSPractice/src/App.jsx
@@ -2,9 +2,16 @@ import EvenOrOddChecker from "./components/EvenOrOddChecker";
 import GuessTheNumber from "./components/GuessTheNumber";
 import LeapYear from "./components/LeapYear";
 import MortageCalculator from "./components/MortageCalculator";
+import { NamedExport } from "./components/ImportExport";
+import DefaultExport from "./components/ImportExport";
 
 const App = () => {
-  return <MortageCalculator />;
+  return (
+    <>
+      <NamedExport />
+      <DefaultExport />
+    </>
+  );
 };
 
 export default App;
---
+++ ReactJSPractice/ReactJSPractice/src/components/ImportExport.jsx
@@ -0,0 +1,9 @@
+const DefaultExport = () => {
+  return <h1>Namaste Default Export 🚀</h1>;
+};
+
+export const NamedExport = () => {
+  return <h1>Namaste Named Export 🚀</h1>;
+};
+
+export default DefaultExport;
