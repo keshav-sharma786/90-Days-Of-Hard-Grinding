9ce367e09db7e74aa975dce5f34ba2f498deba85
Keshav Sharma <Keshav.bhambi@gmail.com>
2026-02-19 20:54:46 +0530

    initial commit

---
+++ NodeJS01/index.js
@@ -0,0 +1,6 @@
+//~ 19th feb, 2026 ==> working on 2 files index.js and app.js
+//~ requiring sum.js file
+debugger
+require("./sum");
+debugger
+console.log("Namaste Require Function");
---
+++ NodeJS01/sum.js
@@ -0,0 +1,12 @@
+debugger
+var x = "Sum Module";
+console.log(x);
+
+function sum() {
+  let a = 10;
+  let b = 20;
+  let sum = a + b;
+  console.log(sum);
+}
+
+sum();
