56c98d0fbe1ab50f0794fdede85c0f74fcb7b3bd
Keshav Sharma <Keshav.bhambi@gmail.com>
2026-02-19 21:06:57 +0530

    initial commit

--- NodeJS01/index.js
+++ NodeJS01/index.js
@@ -1,6 +1,9 @@
 //~ 19th feb, 2026 ==> working on 2 files index.js and app.js
 //~ requiring sum.js file
-debugger
+debugger;
 require("./sum");
-debugger
+debugger;
 console.log("Namaste Require Function");
+//~Let me try to access the variables and functions of sum file/module
+console.log(x);
+sum();
