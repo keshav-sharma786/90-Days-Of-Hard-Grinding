fdf78bdbee317ceeab2564dba3ebf75630a6c2a7
Keshav Sharma <Keshav.bhambi@gmail.com>
2026-02-22 08:49:22 +0530

    initial commit

---
+++ LinkedListMediumHardAndPractice/Merge2SortedListsWithoutDummyNodeMoreOptimised.js
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function (l1, l2) {
+  // Approach2 => without using dummy Node
+  // some corner cases to handle
+  if (!l1) return l2;
+  if (!l2) return l1;
+  let ans = null;
+  let curr = null;
+  // Getting to know the starting point of my
+  // sorted List
+  if (l1.val < l2.val) {
+    ans = l1;
+    l1 = l1.next;
+  } else {
+    ans = l2;
+    l2 = l2.next;
+  }
+  curr = ans;
+  while (l1 && l2) {
+    if (l1.val < l2.val) {
+      curr.next = l1;
+      curr = l1;
+      l1 = l1.next;
+    } else {
+      curr.next = l2;
+      curr = l2;
+      l2 = l2.next;
+    }
+  }
+  if (!l1) {
+    curr.next = l2;
+  }
+  if (!l2) {
+    curr.next = l1;
+  }
+
+  return ans;
+};
