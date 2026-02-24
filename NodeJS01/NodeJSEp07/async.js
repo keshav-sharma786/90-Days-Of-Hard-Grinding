69acbbf5c4c4fe47c0c9e5d1a989a48e8fb871e7
Keshav Sharma <Keshav.bhambi@gmail.com>
2026-02-24 20:04:55 +0530

    initial commit

--- NodeJS01/NodeJSEp07/async.js
+++ NodeJS01/NodeJSEp07/async.js
@@ -1,10 +1,16 @@
 const fs = require("fs");
 const https = require("https");
 
+
 console.log("Hello World");
 
 var a = 1078698;
 var b = 20986;
+
+//~Synchronous Function for reading the file ==> Now this function will actually block the main thread
+const htmlFileData = fs.readFileSync("./file.html", "utf-8"); // 100 ms
+console.log(htmlFileData);
+console.log("This will execute only after file read");
 
 //~Making an api call
 
@@ -14,10 +20,10 @@ https.get("https://dummyjson.com/products/1", (res) => {
 
 setTimeout(() => {
   console.log("setTimeout called after 5 seconds");
-}, 20000);
+}, 30000);
 
 //~Async function
-fs.readFile("./file.txt", (err, data) => {
+fs.readFile("./file.txt", "utf-8", (err, data) => {
   console.log("File Data: ", data);
 });
 
