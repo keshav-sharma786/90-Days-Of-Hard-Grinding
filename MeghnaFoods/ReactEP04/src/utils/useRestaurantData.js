1f674ce3d6dea2bb84d8ec1bd88046f2dbdd940e
Keshav Sharma <Keshav.bhambi@gmail.com>
2026-03-09 19:29:17 +0530

    initial commit

--- MeghnaFoods/ReactEP04/src/utils/useRestaurantData.js
+++ MeghnaFoods/ReactEP04/src/utils/useRestaurantData.js
@@ -3,20 +3,20 @@ import { RES_API } from "./constants";
 import { useState, useEffect } from "react";
 
 const useRestaurantData = () => {
-    const [restaurantInfo, setRestaurantInfo] = useState(null);
-    
-    useEffect(() => {
-       fetchData();
-    }, []);
+  const [restaurantInfo, setRestaurantInfo] = useState(null);
 
-    const fetchData = async () => {
-        const data = await fetch(RES_API);
-        const json = await data.json();
-        console.log(json);
-        setRestaurantInfo(json);
-    }
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    return restaurantInfo;
-}
+  const fetchData = async () => {
+    const data = await fetch(RES_API);
+    const json = await data.json();
+    console.log(json);
+    setRestaurantInfo(json);
+  };
+
+  return restaurantInfo;
+};
 
 export default useRestaurantData;
