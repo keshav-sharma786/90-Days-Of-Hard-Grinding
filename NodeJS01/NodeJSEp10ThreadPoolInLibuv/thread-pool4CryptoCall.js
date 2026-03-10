d63884f0835f4fefa36ac7b8235ec14019381f0f
Keshav Sharma <Keshav.bhambi@gmail.com>
2026-03-10 19:54:30 +0530

    initial commit

--- NodeJS01/NodeJSEp10ThreadPoolInLibuv/thread-pool4CryptoCall.js
+++ NodeJS01/NodeJSEp10ThreadPoolInLibuv/thread-pool4CryptoCall.js
@@ -13,4 +13,10 @@ crypto.pbkdf2("password", "salt", 5000000, 50, "sha512", (err, key) => {
   console.log("2 - cryptoPBKDF2 done");
 });
 
+crypto.pbkdf2("password", "salt", 5000000, 50, "sha512", (err, key) => {
+  console.log("3 - cryptoPBKDF2 done");
+});
 
+crypto.pbkdf2("password", "salt", 5000000, 50, "sha512", (err, key) => {
+  console.log("4 - cryptoPBKDF2 done");
+});
---
+++ NodeJS01/NodeJSEp10ThreadPoolInLibuv/thread-pool5CryptoCall.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+const crypto = require("crypto");
+
+crypto.pbkdf2("password", "salt", 50000000, 50, "sha512", (err, key) => {
+  console.log("1 - cryptoPBKDF2 done");
+});
+
+// fs.readFile("./file.txt", "utf-8", () => {
+//   console.log("File Reading CB");
+// });
+
+crypto.pbkdf2("password", "salt", 50000000, 50, "sha512", (err, key) => {
+  console.log("2 - cryptoPBKDF2 done");
+});
+
+crypto.pbkdf2("password", "salt", 50000000, 50, "sha512", (err, key) => {
+  console.log("3 - cryptoPBKDF2 done");
+});
+
+crypto.pbkdf2("password", "salt", 50000000, 50, "sha512", (err, key) => {
+  console.log("4 - cryptoPBKDF2 done");
+});
+
+crypto.pbkdf2("password", "salt", 50000000, 50, "sha512", (err, key) => {
+  console.log("5 - cryptoPBKDF2 done");
+});
