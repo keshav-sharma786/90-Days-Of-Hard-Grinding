635210c14b724fe1e560d91de5a9e99b78314dff
Keshav Sharma <Keshav.bhambi@gmail.com>
2026-03-11 00:46:32 +0530

    initial commit

---
+++ NodeJS01/NodeJSEp10ThreadPoolInLibuv/Changingthread-poolSize.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+const crypto = require("crypto");
+
+//~command for changing the size of threadPool
+//~$env:UV_THREADPOOL_SIZE = 12; node my_app.js
+
+
+process.env.UV_THREADPOOL_SIZE = 2;
+
+crypto.pbkdf2("password", "salt", 50000000, 50, "sha512", (err, key) => {
+  console.log("1 - cryptoPBKDF2 done");
+});
+
+// fs.readFile("./file.txt", "utf-8", () => {
+//   console.log("File Reading CB");
+// });
+
+crypto.pbkdf2("password", "salt", 50000000, 50, "sha512", (err, key) => {
+  console.log("2 - cryptoPBKDF2 done");
+});
+
+crypto.pbkdf2("password", "salt", 50000000, 50, "sha512", (err, key) => {
+  console.log("3 - cryptoPBKDF2 done");
+});
+
+crypto.pbkdf2("password", "salt", 50000000, 50, "sha512", (err, key) => {
+  console.log("4 - cryptoPBKDF2 done");
+});
+crypto.pbkdf2("password", "salt", 50000000, 50, "sha512", (err, key) => {
+  console.log("5 - cryptoPBKDF2 done");
+});
+
