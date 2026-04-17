3db1716ac92d8f8f3e99cb663ddbd76a2ef56f6a
Keshav Sharma <Keshav.bhambi@gmail.com>
2026-04-17 10:59:06 +0530

    time and space complexity

---
+++ StackAndQueues/RemoveOuterMostParenthesis/Notes.js
@@ -0,0 +1,5 @@
+//&Here Time complexity will be always 0(n) because we always need to traverse the whole string of length n.
+
+//&Stack ==> Stack data structure always take the space complexity of O(n),and your ans also takes the space complexity of O(n)
+
+//?Without Using Stack === now only the ans variable takes the O(n) space Complexity.Though I am not using the stack,the ans variable is taking O(n) space complexity,but I am not using any extra space.So O(1),but if you want to consider this extra ans variable as the extra space then we can say that the space complexity is O(n).Stack can contain O(n) elements inside it.
