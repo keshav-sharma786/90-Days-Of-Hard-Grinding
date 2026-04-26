9cb4d2b5ee5c213cc7b451f1ff5658169ddd4626
Keshav Sharma <Keshav.bhambi@gmail.com>
2026-04-26 23:26:35 +0530

    started with data sanitization and the user validation

---
+++ Nodejs02/DevTinder/src/appRevisionDivingDeepIntoAPI.js
@@ -0,0 +1,159 @@
+import express from "express";
+import { connectDB } from "./config/database.js";
+import { User } from "./models/user.js";
+
+const app = express();
+
+//&Our middleware will now be activated for all the routes.
+app.use(express.json());
+
+app.post("/signup", async (req, res) => {
+  //&But this data cannot be hardcoded right??We basically need a user to fill this data in an HTML form,and then send us the data in an api,we get this data and then we push it into the database.We have to make this data dynamic.
+
+  //?So what we will do is,instead of writing this data over here,I want my data to be sent inside the signup api,while I am making a POST call,I should send the data right from here,and then my api should receive that data and push it into the database.Let us pass the dynamic data into the API.
+
+  //~In the POSTMAN,the top portion represents the request and the below portion is the response.In this top portion,Go to Body ==> This is basically the request body.If I will click on this body,so there are different ways I can send the data inside the API ===  I can either send the form-data,raw data,I can even send the binary data.But I want to send the JSON data to my backend,to my server.My server should read that JSON data and then push it into the database.
+
+  //&In the POSTMAN inside body select the raw.Under raw === under json also you can basically select the lot of things like ==> Text,JSON,javascript,html,XML.You can either send that raw data in text,you can also send the raw data in javascript,you can also send the raw data in json,you can send the raw data in HTML,XML.XML was used widely before JSON.So I will basically send the dynamic data in the form of JSON object from the POSTMAN like this === {"firstName": "Rohit",}
+
+  //?JSON cannot accept the last comma,but a JS object can accept.After sending the data from the POSTMAN,I want to receive that data over here and push it inside my database,How will I do that ???
+
+  // console.log(req); //&This req is the request,this is basically the whole request that POSTMAN has sent to us,and the server has received this request and express has converted this request into an object and it has given this object to us to use it.The data that you sent over here is also the part of the request object but you cannot directly read the data from this req object.Now I want to read the data which is present on the body of this request.I can just do the req.body.
+  //console.log(req.body); //~undefined.Why??Ideally In my req body I have sent this JSON data then why it is coming undefined over here ???the reason is data which is being sent over here is being sent in the JSON format and our server is not able to read that JSON data,to read that JSON data we will need a help of a middleware,why I am saying a middleware?? Because I will have to use it for all my api's.I need a middleware that can just check the incoming request and it can just read the JSON,convert that JSON into a javascript object put it into the body of the request,give us access to that data over here.
+
+  //?There is a very famous middleware which is given to us the expressJS itself, and it is known as express. json middleware.This was also the major concern when we use to develop applications using express,If we want to read some JSON,I can write my own middleware or I have to rely on some random third party middleware.
+
+  //console.log(req.body);
+
+  //&What does this express.json() middleware has done is === It reads the JSON object,converts it into a Javascript object,and it just adds that javascript object back to this req object in the body.Now my req.body is a Javascript object.And now I can also read this body properly.
+
+  //~This req.body is exactly same as that object (dummyUser) which I basically passed inside the new User({})
+
+  //&This JSON data(Rohit Sharma) come over here,this express.json() plugin,a middleware will just read that json object,it will convert it into a javascript object,and will add that javascript object onto the req.body.Now I can read this body.Now I can basically create the new instance of the User model using the data which I have got from the sign-up post api.And this user can now be properly saved to the database.
+
+  const user = new User(req.body);
+  try {
+    await user.save();
+    res.send("User Added successfully");
+  } catch (error) {
+    res.status(400).send("Error saving the user:" + err.message);
+  }
+});
+
+//?Now is the time to read the data also from the database.Let us now make the GET api and in that GET api,I want to get all the users from the database.Basically In the Dev Tinder I basically want to create the feed API.I want to get all the users from the database,It is kind of like my feed.I basically want to create a feed API which will get all the users from the database and give it back to the UI or the POSTMAN.
+
+//~Let us make an api to find only one user from the database.
+//~Get user by email.
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+
+  //&Go to the postman,create a new GET request.Go to the body.Go to the raw and add the JSON data in the form of === {"emailId": "Keshav@gmail.com"} and hit the enter and send the data,you will get that document as the response in the postman.
+
+  //?It will basically give you the array of objects over here.And that array only has one object with the emailId that I passed while sending the request on the POSTMAN.
+
+  //~Suppose that the user with that emailId was not found,How do you handle that case ???Suppose If I passed the wrong email Id??? while sending GET request on the POSTMAN.It will return me an empty array.But what if I have to throw an error ???
+
+  //&What I was doing over here was I was finding the user by using the emailId,what if there were 2 users with the same emailId???Let us first of all create 2 users with the same emailId.Let us create 2 Rohit Sharma's.Now if i send the request,It will give me the 2 objects (Rohit Sharma),but there is also the method findOne() === it will basically return you only the one Object from the database.It will basically not give you an array.Let us create one more GET api for just finding out only the one user.
+
+  try {
+    const users = await User.find({ emailId: userEmail });
+    if (users.length === 0) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(users);
+    }
+  } catch (error) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+//~findOne()
+app.get("/findOneUser", async (req, res) => {
+  const userEmail = req.body.emailId;
+  try {
+    //&Whatever you are passing over here (emailId),basically it should match exactly how you have defined it in your models in the schema,if you have written emailId over here,everywhere you should use emailId.
+
+    //~Suppose if the email is not present then It will basically give me an empty response.inside user null will be stored.
+
+    //?If you donot pass emailId: userEMail inside User.findOne({}),then mongoDB will return the first document present in the collection.But it is not sure,docs also use arbitrary word here.
+    const user = await User.findOne({ emailId: userEmail }).exec();
+    // console.log(user);
+    if (!user) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(user);
+    }
+  } catch (error) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+//&Feed API === GET/feed === get all the users from the database
+app.get("/feed", async (req, res) => {
+  //&Whenever you want to get the data from the database,you should know which model you have to use.
+  //~I am getting users,so I want to query using this model User.Go to docs === mongoose.com/docs/ === Then go to the Model in the left side secion.Go to the Model.find() method.Suppose If I want to find out the documents in my database,this is how I can find it.
+
+  //?Suppose If I want to find out all the documents inside the collection
+
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (error) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+//!Let us now make the delete API.first of all let us find in the documentation what query I will use for deleting a single document.So i can use this method === findByIdAndDelete
+
+//!I also need a userId of the user document to be deleted,how will I get the userId ??
+app.delete("/user", async (req, res) => {
+  console.log(req.body);
+  console.log(req.body.userId);
+  const userId = req.body.userId;
+  console.log(userId);
+  try {
+    const user = await User.findByIdAndDelete({ _id: userId });
+    console.log(user);
+    res.send("User deleted successfully");
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+//&Let us create an api for updating the data of the user === update/edit route
+//&We will basically use findById And update query.
+app.patch("/user", async (req, res) => {
+  console.log(req.body);
+  const userId = req.body.userId;
+  //&this is basically the updated data that I will sent from the POSTMAN
+  //&Go to Postman and send the request like this ===
+  console.log(userId);
+  const data = req.body;
+  console.log(data);
+  try {
+    //&If you are trying to update a field which is not present in your schema then mongoDB will simply ignore it.that field/data will not be added inside the document.
+
+    //&Passing the third parameter also which is options,options is basically an object.
+    const updatedUser = await User.findByIdAndUpdate({ _id: userId }, data, {
+      returnDocument: "after",
+    });
+    console.log(updatedUser);
+    res.send("User updated successfully");
+  } catch (error) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+connectDB()
+  .then((res) => {
+    console.log("Database connection established...");
+
+    // console.log(res);
+
+    app.listen(7777, () => {
+      console.log("App is listening on Port 7777...");
+    });
+  })
+  .catch((err) => {
+    console.log("Database cannot be connected");
+  });
--- Nodejs02/DevTinder/src/models/user.js
+++ Nodejs02/DevTinder/src/models/user.js
@@ -1,19 +1,41 @@
 import mongoose from "mongoose";
 
-//&here I will define what a user in our database is ??What fields this user collection will have ???All those things will be defined over here.
-
-//&Schema is a method which is basically defined inside the mongoose object.I have to pass some object inside it as an argument.
-
 const userSchema = new mongoose.Schema({
-  //~here pass all the parameters that define the user.This is basically how you define a schema.This schema basically tells you what all information about the user are we stroing into our database.These are the legit fields that can be present inside the user's collection.
+  //?we will add some strict checks over here in the database and basically if those checks are not met,we will not insert the document into our database.
+
+  //*Go to the official documentation of mongoose, on the right hand side go to the schemaTypes over there.This basically tells you more about information about what can be the different types inside your schema.
+
+  //&If I want that there should be some mandatory fields inside our database.If those fields are not there,we will not create our user document.What can be these fields.It can be email,It can be password,It can be firstName, lastName these are kind of like required fields,these are mandatory fields which are required to sign up a user.Suppose if a user is signing up you should atleast know his firstName,emailId and password right?Otherwise How would you register a user onto the platform??These are mandatory fields.
+
+  //~you can basically add a required field over here.
+
+  //~ I want If a user registers onto our platform then he should definitely have a firstName and he should definitely have a lastName.
+
+  //?Another important check is basically a unique check, suppose if I want in my database that there should be only one entry with one email id,there cannot be the 2 different users with the same email id.for that you have to keep emailId as the unique field.you can basically pass a unique flag.This unique flag is a also a boolean flag over here.
+
+  //*Suppose if i also want to add a default value,suppose If I want that whenever a user registers so it should take some default value.When I will create the userSchema, it can also take a photoUrl.So let us also store a photoUrl.it is basically the link to the photograph of the user.
+
+  //todo=== there can also be a kind of like a short description basically like about section or on instagram you might have seen that bio, headline on the linkedin.Something like that.Let us also add that about field inside my userSchema.Let us also add one more field i.e skills.And you can also add a default value for some users.
+
+  //&Whenever you will have schema,with [String] i.e array of strings or something, so mongoDB by default creates an empty space for your array so basically that's a default behaviour.
+
+  //&Whenever a new user will register,without an About Section,so the default About will be the default value which we have added in the schema.Similarly you can also add a default photo url.
+
+  //~Suppose If somebody is entering their emailId in the capital letters like inside the random cases.To keep ypur emailId's consistent,you can add lowercase flag into the emailId.
+
+  //~Similarly I can also add much more  checks
+
   firstName: {
     type: String,
+    //!Now user will definitely have to give a firstName, otherwise mongoose will not allow the insertion into the database.Mongoose will not allow the insertion of documents into the collection.
+    required: true,
   },
   lastName: {
     type: String,
   },
   password: {
     type: String,
+    required: true,
   },
   age: {
     type: Number,
@@ -25,30 +47,27 @@ const userSchema = new mongoose.Schema({
   emailId: {
     // ← was completely missing!
     type: String,
+    //!Now whatever case user may use,but the email id will be stored in the lowercase format.Whatever the user is sending, we will just store it how we want to,I want all my email id's to be unique , they should always be in lowercase
+    lowercase: true,
+    required: true,
+    //! unique:true will not let me insert the same emailId into my database.If I will try to pass then mongoose will throw this error === Error saving the user:E11000 duplicate key error collection: devTinder.users index: emailId_1 dup key: { emailId:
+
+    unique: true,
+  },
+  photoUrl: {
+    type: String,
+    //&Adding a default image url over here.So if you dont give a photo then by default it will take this below photo url.Let us enter Mark Zuckerberg into our database.
+    default: "https://geographyandyou.com/images/user-profile.png",
+  },
+  about: {
+    type: String,
+    //~Adding a default value
+    default: "This is a default description about the user",
+  },
+  skills: {
+    //& user can have multiple skills, a user can have javascript, java full stack, nodeJS and so on.It is kind of like a array of skills.
+    type: [String],
   },
 });
 
-//console.log(userSchema);
-
-//&Step2 ==> Once you create the schema then we will create the mongoose model.We will create a "User" model So write "User".first thing you will pass is the name of the model and the second thing you will pass is the schema
-
 export const User = mongoose.model("User", userSchema);
-
-// console.log(User);
-// console.log(User.prototype);
-
-//?Whenever you create a database a mongoDB database,you create collection inside it,that collection will basically hold some data.
-
-//~Whenever you are creating a model,the name always starts with a capital letter,basically name when it starts with a capital letter,says that okay it is a kind of like a mongoose model.It is a User,it is defining a collection.
-
-//&This is like a class,User class and then we will create small new instances (objects) of that class,whenever a new User will come in it will be a new type of instance (object) of that model,this userSchema is basically the definition of the model.this userSchema defines this model.
-
-//~This userModel is basically a class which will create it's own instances (objects),this model will create it's own instances.
-
-//?Suppose if a user Rahul came onto your website,so it is of type user and it will go into the user collection.
-
-//&with the help of this User model I will now create new-new instances.Suppose if I need to add a new user,so I will create a new instance of this User model and I will put it into the database.
-
-//&Now you can create a API to add a user into the database.
-
-//&We will basically create a POST api.We will basically create an API which will sign up the user.Let us meet again in the app.js
